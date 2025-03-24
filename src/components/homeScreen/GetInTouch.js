febd9aa3d3553804451867a153a82ed9d69b5b59
VINAMRA SHREY <19341012+vs47096@users.noreply.github.com>
2025-03-24 22:26:56 +0530

    Corrected get in touch section

--- src/components/homeScreen/GetInTouch.js
+++ src/components/homeScreen/GetInTouch.js
@@ -23,15 +23,16 @@ export default class GetInTouch extends Component {
 
         return (
             <div className='align-items-center app-base-font-family flex flex-column gap-5 px-8 py-5' style={backgroundStyle}>
-                <div className='flex justify-content-center text-6xl text-white'>
+                <div className='flex justify-content-center text-5xl text-white'>
                     Get in touch with us
                 </div>
                 <div className=''>
-                    <InputText value={this.state.emailAddress} onChange={(e) => this.setState({ emailAddress: e.target.value })} className='w-24rem' 
+                    <InputText value={this.state.emailAddress} onChange={(e) => this.setState({ emailAddress: e.target.value })} className='w-21rem text-xs' 
                         placeholder='Enter your email and we will get back to you'/>
                 </div>
                 <div className=''>
-                    <Button label="Send" className='app-background-linear border-round-3xl font-semibold p-button p-button-text p-component px-8 py-3 text-lg text-white-alpha-90 border-none' />
+                    <Button label="Submit" className='app-background-linear border-round-3xl font-semibold px-8 py-3 text-base text-white-alpha-90 border-none'
+                         />
                 </div>
             </div>
         );
