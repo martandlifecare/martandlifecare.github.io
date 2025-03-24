21a83fc5ae350958e1ae8f1820dd86a031e1adf7
VINAMRA SHREY <19341012+vs47096@users.noreply.github.com>
2025-03-25 00:45:37 +0530

    Implemented get in touch

--- src/components/homeScreen/GetInTouch.js
+++ src/components/homeScreen/GetInTouch.js
@@ -8,8 +8,27 @@ export default class GetInTouch extends Component {
     constructor() {
         super();
         this.state = {
-            emailAddress: ''
+            query: ''
         }
+    }
+
+    sendEmail = () => {
+        const recipient = 'martandlifecare@gmail.com';
+        const subject = `Query from website - ${new Date().toLocaleDateString('en-GB').split('/').join('-')}`; // Format as DD-MM-YYYY
+        const query = this.state.query;
+        const body = encodeURIComponent(`Hi,\n\nBelow is a query submitted, please have a look:\n\n${query}`); // Modified body
+
+        const mailtoLink = `mailto:${recipient}?subject=${encodeURIComponent(subject)}&body=${body}`;
+
+        // Programmatically create and click an invisible link
+        const tempLink = document.createElement('a');
+        tempLink.href = mailtoLink;
+        tempLink.style.display = 'none';
+        document.body.appendChild(tempLink);
+        tempLink.click();
+        document.body.removeChild(tempLink);
+
+        this.setState({query: ''})
     }
 
     render() {
@@ -27,12 +46,12 @@ export default class GetInTouch extends Component {
                     Get in touch with us
                 </div>
                 <div className=''>
-                    <InputText value={this.state.emailAddress} onChange={(e) => this.setState({ emailAddress: e.target.value })} className='w-21rem text-xs' 
-                        placeholder='Enter your email and we will get back to you'/>
+                    <InputText value={this.state.query} onChange={(e) => this.setState({ query: e.target.value })} className='w-30rem text-xs' 
+                        placeholder='Write your query and please mention your email/contact number.'/>
                 </div>
                 <div className=''>
                     <Button label="Submit" className='app-background-linear border-round-3xl font-semibold px-8 py-3 text-base text-white-alpha-90 border-none'
-                         />
+                        onClick={(e) => this.sendEmail()} />
                 </div>
             </div>
         );
